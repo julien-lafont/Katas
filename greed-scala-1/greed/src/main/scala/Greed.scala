efa05bd9228b06e5dadd78e2bd45773529db0a3d
Julien Lafont <yotsumi.fx@gmail.com>
2012-07-01 00:10:07 +0200

    Mise à jour KATA greed

--- greed-scala-1/greed/src/main/scala/Greed.scala
+++ greed-scala-1/greed/src/main/scala/Greed.scala
@@ -6,16 +6,15 @@ object Greed {
 
 		if (numbers.size != 5) throw new IllegalArgumentException("5 dices are required")
 		
-		//val grouped = numbers.groupBy(i => i).mapValues(_.size)
-		val grouped = 0.to(6).map(i => numbers.count(_==i))
+		val grouped = numbers.groupBy(i => i).mapValues(_.size)
 	
-		val scoreTriple = grouped.zipWithIndex.foldLeft(0)((acc, i) => 
-			if (i._1 >= 3) acc + i._2 * 
-					(if (i._2 == 1) 1000 else 100) 
+		val scoreTriple = grouped.foldLeft(0)((acc, i) => 
+			if (i._2 >= 3) acc + i._1 * 
+					(if (i._1 == 1) 1000 else 100) 
 			else acc)
 			
-		val score1 = grouped(1) % 3 * 100
-		val score5 = grouped(5) % 3 * 50
+		val score1 = grouped.getOrElse(1, 0) % 3 * 100
+		val score5 = grouped.getOrElse(5, 0) % 3 * 50
 
 		scoreTriple + score1 + score5
 	}
