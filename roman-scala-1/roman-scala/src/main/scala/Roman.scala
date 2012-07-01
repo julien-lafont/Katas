6b1d6d4f611777d9de08ef3cffecc86ce14a87f2
Julien Lafont <yotsumi.fx@gmail.com>
2012-07-01 13:38:47 +0200

    Ajout d'une version récursive du convertisseur

--- roman-scala-1/roman-scala/src/main/scala/Roman.scala
+++ roman-scala-1/roman-scala/src/main/scala/Roman.scala
@@ -4,21 +4,25 @@ import scala.collection.immutable._
 
 object Roman {
 
-	private val table = ListSet(
-		1000 -> "M", 900 -> "CM", 
-		500  -> "D", 400 -> "CD", 
-		100  -> "C", 90  -> "XC", 
-		50   -> "L", 40  -> "XL",
-		10   -> "X", 9   -> "IX",
-		5    -> "V", 4   -> "IV",
-		1    -> "I")
+	private val table = List(
+		(1000 , "M"),  (900 , "CM"), 
+		(500  , "D"),  (400 , "CD"), 
+		(100  , "C"),  (90  , "XC"), 
+		(50   , "L"),  (40  , "XL"),
+		(10   , "X"),  (9   , "IX"),
+		(5    , "V"),  (4   , "IV"),
+		(1    , "I"))
 
-	// 2ème essai : boucle for simplifiée grâce à la fonction ’String.*(times)’
-	def apply(num: Int) : String = {
+	def apply(num: Int) = convertWithFold(num)
 
-		val (roman, _) = table.foldRight(("", num))((tuple, result) => {
+	// ------------------
+	// Première version à base de foldLeft
+	// ------------------
+	def convertWithFold(num: Int) : String = {
+
+		val (roman, _) = table.foldLeft(("", num))((result, tuple) => {
 			val (arabicNumber, romanNumber) = tuple
-			val (romanString, remainder)  = result
+			val (romanString, remainder)    = result
 
 			val romanUnit = romanNumber * (remainder / arabicNumber)
 			(romanString + romanUnit, remainder % arabicNumber) 
@@ -28,8 +32,19 @@ object Roman {
 	}
 
 	// ------------------
+	// Seconde version avec récursivité
+	// ------------------
+	def convertWithRec(num: Int) : String = {
+		def convert(value: Int, table: List[(Int,String)]) : String = table.headOption match {
+			case None => ""
+			case Some(tuple) => tuple._2 * (value / tuple._1) + convert(value % tuple._1, table.tail)
+		}
+		convert(num, table)
+	}
 
+	// ------------------
 	// Version One-Liner
-	def convert(n: Int) = table.:\(("", n))((e, r) => (r._1 + (e._2 * (r._2 / e._1)), r._2 % e._1 ))._1
+	// ------------------
+	def convertOneLiner(n: Int) = table./:(("", n))((r, e) => (r._1 + (e._2 * (r._2 / e._1)), r._2 % e._1 ))._1
 	
 }
